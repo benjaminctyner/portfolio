b4ad673b6d1140f4406b2f4833bec091a912f811
Ben <benjaminctyner@gmail.com>
2023-06-16 08:26:56 -0400

    build typo.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915eff]'>Ben</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop websites
+            I build websites
             <br className='sm:block hidden' /> and web applications.
           </p>
         </div>
