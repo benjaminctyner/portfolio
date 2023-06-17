1de356f4b2d7655a1473c66b8d9b12eaec985b68
Ben <benjaminctyner@gmail.com>
2023-06-17 19:48:10 -0400

    3 cards.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ const ServiceCard = ({ index, title, icon }) => (
 );
 
 const About = () => {
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1000px)' });
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1200px)' });
   const smallServices = services.slice(0, -1);
   return (
     <>
