fc38afa8b63bb3a1c78c3cc1082f6af8d0cc8be2
Ben <benjaminctyner@gmail.com>
2023-06-18 17:24:01 -0400

    Fix about page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,24 +8,9 @@ import { SectionWrapper } from '../hoc';
 
 import { useMediaQuery } from 'react-responsive';
 
-const ServiceCard = ({ index, title, icon, isTabletOrMobile }) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
     <>
-      {isTabletOrMobile && (
-        <div className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-          <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
-            <img
-              src={icon}
-              alt='web-development'
-              className='w-16 h-16 object-contain'
-            />
-
-            <h3 className='text-white text-[20px] font-bold text-center'>
-              {title}
-            </h3>
-          </div>
-        </div>
-      )}
       <Tilt className='xs:w-[250px] w-full'>
         <motion.div
           variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
