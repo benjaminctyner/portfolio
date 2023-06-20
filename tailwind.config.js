0b0b841c0c61a231dd44f84692efc5e3cbc8e1f6
Ben <benjaminctyner@gmail.com>
2023-06-20 10:10:30 -0400

    tailwind fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,jsx}', './src/*.{js,jsx}'],
   mode: 'jit',
   theme: {
     extend: {
